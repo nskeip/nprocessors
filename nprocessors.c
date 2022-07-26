3c2378fe27f4f011df924f32e4ae020f42a380d7
Nikita Hismatov <hismatovns@gmail.com>
2022-07-26 21:53:11 +0500

    number of available processors on Windows

--- nprocessors.c
+++ nprocessors.c
@@ -1,13 +1,23 @@
 #ifdef __linux__
 #include <sys/sysinfo.h>
 #define nprocs() get_nprocs()
+#elif _WIN32
+#include <sysinfoapi.h>
+unsigned int nprocs(void) {
+    SYSTEM_INFO info;
+    GetSystemInfo(&info);
+    if(info.dwNumberOfProcessors < 1) {
+        return 4;
+    }
+    return info.dwNumberOfProcessors;
+}
 #else
 #define nprocs() 4
 #endif
 #include <stdio.h>
 
 int main(void) {
-  int n = nprocs();
+  unsigned int n = nprocs();
   printf("Number of processors available: %d\n", n);
   return 0;
 }
